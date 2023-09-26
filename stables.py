3f655c79b307d677acc06076bb756e57e114791a
Log131 <87196980+Log131@users.noreply.github.com>
2023-09-26 15:39:56 +0300

    Add files via upload

--- stables.py
+++ stables.py
@@ -21,49 +21,32 @@ async def softs_5(init, promts, scale, safety):
     "prompt": promts,
     
     "negative_prompt": "High pass filter, airbrush,portrait,zoomed, soft light, smooth skin,closeup, Anime, fake, cartoon, deformed, extra limbs, extra fingers, mutated hands, bad anatomy, bad proportions , blind, bad eyes, ugly eyes, dead eyes, blur, vignette, out of shot, out of focus, gaussian, closeup, monochrome, grainy, noisy, text, writing, watermark, logo, oversaturation , over saturation, over shadow, floating limbs, disconnected limbs, anime, kitsch, cartoon, penis, fake, (black and white), airbrush, drawing, illustration, boring, 3d render, long neck, out of frame, extra fingers, mutated hands, monochrome, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, glitchy, bokeh, (((long neck))), (child), (childlike), 3D, 3DCG, cgstation, red eyes, multiple subjects, extra heads, close up, man, ((asian)), text, bad anatomy, morphing, messy broken legs decay, ((simple background)), deformed body, lowres, bad anatomy, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low jpeg artifacts, signature, watermark, username, blurry, out of focus, old, amateur drawing, odd, fat, morphing, ((simple background)), artifacts, signature, artist name, [blurry], disfigured, mutated, (poorly hands), messy broken legs, decay, painting, duplicate, closeup",
-  
-    "model_id": "midjourney",
-    "init_image": init,
-  
     
-  
+    "init_image": init,
+    "num_inference_steps": "30",
+    "safety_checker": safety,
+    "guidance_scale": scale,
     "width": "512",
     "height": "512",
     "samples": "1",
     "num_inference_steps": "30",
-    "safety_checker": safety,
     "enhance_prompt": "yes",
-  
-    
-    "guidance_scale": scale,
     "strength": 0.7,
-  
-    
-    
-    
-    
     "seed": None,
-    "webhook": None,
-    "track_id": None,
-    
-    "scheduler": "UniPCMultistepScheduler",
-    "lora_model": None,
-    "tomesd": "yes",
-    "use_karras_sigmas": "yes",
-    "vae": None,
-    "lora_strength": None,
-    "embeddings_model": None,
     "webhook": None,
     "track_id": None
     
     
     }
-
     datas = json.dumps(s)
+    
+    
+    
+    
     headers = {
     'Content-Type': 'application/json'
     }
-    res = requests.post('https://stablediffusionapi.com/api/v4/dreambooth/img2img',headers=headers, data=datas)
+    res = requests.post('https://stablediffusionapi.com/api/v3/img2img',headers=headers, data=datas)
 
 
 
@@ -94,35 +77,29 @@ async def softstexts(promts,safety,scale):
     "prompt": promts,
     
     "negative_prompt": "High pass filter, airbrush,portrait,zoomed, soft light, smooth skin,closeup, Anime, fake, cartoon, deformed, extra limbs, extra fingers, mutated hands, bad anatomy, bad proportions , blind, bad eyes, ugly eyes, dead eyes, blur, vignette, out of shot, out of focus, gaussian, closeup, monochrome, grainy, noisy, text, writing, watermark, logo, oversaturation , over saturation, over shadow, floating limbs, disconnected limbs, anime, kitsch, cartoon, penis, fake, (black and white), airbrush, drawing, illustration, boring, 3d render, long neck, out of frame, extra fingers, mutated hands, monochrome, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, glitchy, bokeh, (((long neck))), (child), (childlike), 3D, 3DCG, cgstation, red eyes, multiple subjects, extra heads, close up, man, ((asian)), text, bad anatomy, morphing, messy broken legs decay, ((simple background)), deformed body, lowres, bad anatomy, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low jpeg artifacts, signature, watermark, username, blurry, out of focus, old, amateur drawing, odd, fat, morphing, ((simple background)), artifacts, signature, artist name, [blurry], disfigured, mutated, (poorly hands), messy broken legs, decay, painting, duplicate, closeup",
-    "model_id": "midjourney",
-  
     "width": "512",
     "height": "512",
     "samples": "1",
-    "num_inference_steps": "30",
-    "safety_checker": safety,
-    "enhance_prompt": "yes",
-    "num_inference_steps": "30",
+    "num_inference_steps": "20",
     "seed": None,
+    "guidance_scale": 7.5,
     "multi_lingual": "no",
     "panorama": "no",
     "self_attention": "no",
     "upscale": "no",
     "embeddings_model": None,
-    "lora_model": None,
-    "tomesd": "yes",
-    "use_karras_sigmas": "yes",
-    "vae": None,
-    "lora_strength": None,
-    "scheduler": "UniPCMultistepScheduler",
     "webhook": None,
     "track_id": None,
+  
+    "safety_checker": safety,
+    
     "guidance_scale": scale
 
     }
 
     
-    
+   
+
     
     
     
@@ -137,7 +114,7 @@ async def softstexts(promts,safety,scale):
     
     
     }
-    res = requests.post('https://stablediffusionapi.com/api/v4/dreambooth',headers=headers, data=datas)
+    res = requests.post('https://stablediffusionapi.com/api/v3/text2img',headers=headers, data=datas)
 
     result = json.loads(res.text)
     
